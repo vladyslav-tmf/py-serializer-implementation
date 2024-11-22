c40a8994dfc79b2e9d2f50d028bc56f73dd4f29b
Vladyslav Timofieiev <vladislav.tmf@gmail.com>
2024-11-22 21:39:26 +0200

    Add car model migration and implement serialize_car_object & deserialize_car_object functions
    - Created initial database migration for the car model.
    - Adjusted timezone setting in the project configuration to "Europe/Kiev".
    - Implemented serialization and deserialization functions for the Car model using Django REST Framework.

--- main.py
+++ main.py
@@ -1,9 +1,18 @@
+import io
+
+from rest_framework.parsers import JSONParser
+from rest_framework.renderers import JSONRenderer
+
 from car.models import Car
+from car.serializers import CarSerializer
 
 
 def serialize_car_object(car: Car) -> bytes:
-    pass
+    return JSONRenderer().render(CarSerializer(car).data)
 
 
 def deserialize_car_object(json: bytes) -> Car:
-    pass
+    data = JSONParser().parse(io.BytesIO(json))
+    serializer = CarSerializer(data=data)
+    serializer.is_valid(raise_exception=True)
+    return serializer.save()
